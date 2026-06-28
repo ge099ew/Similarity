3756d1a2b96fb2ac1fea2022b66fd17cbfac1423
ge099ew <a1w2m43@gmail.com>
2026-06-28 18:30:48 +0900

    Similarity プロトタイプv0.1

--- benchmark/bench_fib.iia.s
+++ benchmark/bench_fib.iia.s
@@ -2,11 +2,36 @@
 .balign 16
 fibonacci:
 	endbr64
-	movl %edi, %eax
-	cmpl $1, %eax
+	pushq %rbp
+	movq %rsp, %rbp
+	pushq %rbx
+	pushq %r12
+	movl %edi, %ebx
+	cmpl $1, %ebx
 	jle .Lbb2
-	movl $0, %eax
+	subq $16, %rsp
+	movq %rsp, %r12
+	movl %ebx, %edi
+	subl $1, %edi
+	callq fibonacci
+	xchgl %eax, %ebx
+	movl %ebx, (%r12)
+	subq $16, %rsp
+	movq %rsp, %r12
+	movl %eax, %edi
+	subl $2, %edi
+	callq fibonacci
+	movl %eax, (%r12)
+	addl %ebx, %eax
+	jmp .Lbb3
 .Lbb2:
+	movl %ebx, %eax
+.Lbb3:
+	movq %rbp, %rsp
+	subq $16, %rsp
+	popq %r12
+	popq %rbx
+	leave
 	ret
 .type fibonacci, @function
 .size fibonacci, .-fibonacci
